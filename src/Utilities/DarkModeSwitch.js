fc8f37cfd2c68e796401adf21be1d5e37f5e95fc
Wenze Ma <maw1@rose-hulman.edu>
2022-07-11 03:10:01 -0400

    dark mode

--- src/Utilities/DarkModeSwitch.js
+++ src/Utilities/DarkModeSwitch.js
@@ -9,12 +9,13 @@ const DarkModeSwitch = styled(Switch)(({ theme }) => ({
         padding: 0,
         transform: 'translateX(6px)',
         '&.Mui-checked': {
-            color: '#fff',
+            // color: '#fff',
+            // backgroundColor: '#fff',
             transform: 'translateX(22px)',
             '& .MuiSwitch-thumb:before': {
-                backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-                    '#fff',
-                )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+                backgroundColor: '#fff',
+                borderRadius: '50%',
+                backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
             },
             '& + .MuiSwitch-track': {
                 opacity: 1,
