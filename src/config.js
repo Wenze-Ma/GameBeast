e71008377a114682f364f345ad0afe4271d606c6
Wenze Ma <maw1@rose-hulman.edu>
2022-07-17 20:36:48 -0400

    dev env

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
     ? "http://localhost:5000"
-    : "http://137.184.100.179:6000";
+    : "http://137.184.100.179:5399";
