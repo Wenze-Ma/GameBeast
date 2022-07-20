5ce4a9dfc5821ccca524fd1120815e0ceb929a52
Wenze Ma <maw1@rose-hulman.edu>
2022-07-20 01:17:55 -0400

    test google login

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
     ? "http://localhost:5000"
-    : "https://be.gameonstar.xyz/";
+    : "https://www.gameonstar.xyz";
