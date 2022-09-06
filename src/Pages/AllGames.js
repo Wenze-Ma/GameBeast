a9cc6a60e68abdc5aa7ece9371fd5563f1a78ca3
Wenze Ma <maw1@rose-hulman.edu>
2022-09-06 17:12:02 -0500

    changed descriptions and images

--- src/Pages/AllGames.js
+++ src/Pages/AllGames.js
@@ -2,16 +2,16 @@ import './pages.css'
 import GameCard from "../Components/GameCard/GameCard";
 import Utilities from "../Utilities/Utilities";
 import Game from "../Models/Game";
-
 export const allGames = [
-    new Game('Tic Tac Toe', 'https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/nastuh.jpg',
-        'Game', 'stupid game', 'tic-tac-toe', [2, 2],
+    new Game('Tic Tac Toe', require('../Images/Logos/ttt.png'),
+        'Game', 'Tic-tac-toe is a paper-and-pencil game for two players who take turns marking the spaces in a three-by-three grid with X or O. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner.',
+        'tic-tac-toe', [2, 2],
         {
             board: Array(9).fill(''),
             state: 'X_TURN',
         }),
-    new Game('Wordle', 'https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/nastuh.jpg',
-        'Game', 'another stupid game',
+    new Game('Wordle', require('../Images/Logos/wordle.png'),
+        'Game', 'Wordle is a web-based word game created and developed by Welsh software engineer Josh Wardle, and owned and published by The New York Times Company since 2022. Players have six attempts to guess a five-letter word, with feedback given for each guess in the form of colored tiles indicating when letters match or occupy the correct position.',
         'wordle', [2, 10],
         {
             board: Array(9).fill(''),
